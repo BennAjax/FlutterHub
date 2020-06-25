77e1a1a75d324a4e445266b5ad8c7fe1a6c7570f
BennAjax <bennkeys1@gmail.com>
2020-06-25 23:05:17 +0100

    kitten app

--- lib/main.dart
+++ lib/main.dart
@@ -2,193 +2,139 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(FriendlyChatApp());
-}
+void main() => runApp(const KittenApp());
 
-final ThemeData kIOSTheme = ThemeData(
-  primarySwatch: Colors.orange,
-  primaryColor: Colors.grey[100],
-  primaryColorBrightness: Brightness.light,
-);
+class KittenApp extends StatelessWidget {
+  const KittenApp();
 
-final ThemeData kDefaultTheme = ThemeData(
-  primarySwatch: Colors.purple,
-  accentColor: Colors.orangeAccent[400],
-);
-
-class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FriendlyChat',
-        theme: defaultTargetPlatform == TargetPlatform.iOS // NEW
-            ? kIOSTheme                                      // NEW
-            : kDefaultTheme,
-        home: ChatScreen()
-    );
+        title: 'kitten App',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            buttonColor: Colors.purple,
+            buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary)),
+        home: HomePage());
   }
 }
 
-class ChatScreen extends StatefulWidget {
-  @override
-  _ChatScreenState createState() => _ChatScreenState();
+// Because IOS Simulators and Android emulators use different addresses to represent the machine they are running on
+final String server = defaultTargetPlatform == TargetPlatform.android
+    ? "192.168.43.253"
+    : "localhost";
+
+class Kitten {
+  const Kitten({this.name, this.description, this.age, this.imageUrl});
+
+  final String name;
+  final String description;
+  final int age;
+  final String imageUrl;
 }
 
-class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
-  final _textController = TextEditingController();
-  final List<ChatMessage> _messages = [];
-  final FocusNode _focusNode = FocusNode();
-  bool _isComposing = false;
+final List<Kitten> _kittens = <Kitten>[
+  Kitten(
+    name: 'Mittens',
+    description:
+        'This pinnacle of cats. When Mittens sits in your lap, you feel like royalty.',
+    age: 11,
+    imageUrl: 'kitten0.jpg',
+  ),
+  Kitten(
+    name: 'Fluffy',
+    description: 'World\'s cutest kitten, Seriously, we did the research',
+    age: 3,
+    imageUrl: 'kitten1.jpg',
+  ),
+  Kitten(
+    name: 'Scooter',
+    description: 'Chases string faster than 9/10 competing kittens',
+    age: 2,
+    imageUrl: 'kitten2.jpg',
+  ),
+  Kitten(
+    name: 'Steve',
+    description: 'Steve is cool and just kind of hangs out',
+    age: 4,
+    imageUrl: 'kitten3.jpg',
+  ),
+  Kitten(
+    name: 'Mikky',
+    description: 'Cool like a sheep but furious as a tiger',
+    age: 6,
+    imageUrl: 'kitten4.png',
+  )
+];
 
+Widget _dialogBuilder(BuildContext context, Kitten kitten) {
+  final ThemeData themeData = Theme.of(context);
+  return SimpleDialog(
+    contentPadding: EdgeInsets.zero,
+    children: <Widget>[
+      Image.asset('assets/images/${kitten.imageUrl}', fit: BoxFit.fill),
+      Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              kitten.name,
+              style: themeData.textTheme.headline4,
+            ),
+            Text('${kitten.age} months old',
+                style: themeData.textTheme.subtitle1
+                    .copyWith(fontStyle: FontStyle.italic)),
+            SizedBox(height: 16.0),
+            Text(kitten.description, style: themeData.textTheme.bodyText1),
+            SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Wrap(
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () => {Navigator.of(context).pop()},
+                    child: Text('I\'M ALLERGIC'),
+                  ),
+                  RaisedButton(
+                    onPressed: () => {},
+                    child: Text('ADOPT'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+Widget _listItemBuilder(BuildContext context, int index) {
+  return GestureDetector(
+    onTap: () => showDialog(
+        context: context,
+        builder: (context) => _dialogBuilder(context, _kittens[index])),
+    child: Container(
+      padding: const EdgeInsets.only(left: 16.0),
+      alignment: Alignment.centerLeft,
+      child: Text(_kittens[index].name,
+          style: Theme.of(context).textTheme.headline),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Friendly Chat'),
-        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        title: Text('Kitten Catalog'),
       ),
-      body: Container(
-        child: Column(
-          // MODIFIED
-          children: [
-            // NEW
-            Flexible(
-              // NEW
-              child: ListView.builder(
-                // NEW
-                padding: EdgeInsets.all(8.0), // NEW
-                reverse: true, // NEW
-                itemBuilder: (_, int index) => _messages[index], // NEW
-                itemCount: _messages.length, // NEW
-              ), // NEW
-            ), // NEW
-            Divider(height: 1.0), // NEW
-            Container(
-              // NEW
-              decoration:
-                  BoxDecoration(color: Theme.of(context).cardColor), // NEW
-              child: _buildTextComposer(), //MODIFIED
-            ), // NEW
-          ], // NEW
-        ),
-          decoration: Theme.of(context).platform == TargetPlatform.iOS // NEW
-              ? BoxDecoration(          // NEW
-            border: Border(       // NEW
-              top: BorderSide(color: Colors.grey[200]), // NEW
-            ),                    // NEW
-          )                       // NEW
-              : null),
-    );// NEW
-  }
-
-  Widget _buildTextComposer() {
-    return IconTheme(
-      data: IconThemeData(color: Theme.of(context).accentColor),
-      child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: <Widget>[
-              Flexible(
-                child: TextField(
-                  controller: _textController,
-                  onChanged: (String text) {
-                    setState(() {
-                      _isComposing = text.length > 0;
-                    });
-                  },
-                  onSubmitted: _isComposing ? _handleSubmitted : null, //
-                  decoration:
-                      InputDecoration.collapsed(hintText: 'Send a message'),
-                  focusNode: _focusNode,
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: 4.0),
-                  child: Theme.of(context).platform == TargetPlatform.iOS ? // MODIFIED
-                  CupertinoButton(                                      // NEW
-                    child: Text('Send'),                                // NEW
-                    onPressed: _isComposing                             // NEW
-                        ? () =>  _handleSubmitted(_textController.text) // NEW
-                        : null,) :   IconButton(
-                    icon: const Icon(Icons.send),
-                    onPressed: _isComposing // MODIFIED
-                        ? () =>
-                            _handleSubmitted(_textController.text) // MODIFIED
-                        : null,
-                  ))
-            ],
-          )),
-    );
-  }
-
-  void _handleSubmitted(String text) {
-    _textController.clear();
-    setState(() {                             // NEW
-      _isComposing = false;                   // NEW
-    });
-    ChatMessage message = ChatMessage(
-      //NEW
-      text: text,
-      animationController: AnimationController(
-        // NEW
-        duration: const Duration(milliseconds: 700), // NEW
-        vsync: this, // NEW
-      ), //NEW
-    ); //NEW
-    setState(() {
-      //NEW
-      _messages.insert(0, message); //NEW
-    });
-    _focusNode.requestFocus(); // Add focus to textfield after submission
-    message.animationController.forward();
-  }
-
-  @override
-  void dispose() {
-    for (ChatMessage message in _messages)
-      message.animationController.dispose();
-    super.dispose();
-  }
-}
-
-class ChatMessage extends StatelessWidget {
-  final String text;
-  final AnimationController animationController;
-
-  ChatMessage({this.text, this.animationController}); // NEW
-
-  @override
-  Widget build(BuildContext context) {
-    String _name = 'Benn';
-
-    return SizeTransition(
-      sizeFactor:
-          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
-      axisAlignment: 0.0,
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: CircleAvatar(child: Text(_name[0])),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(_name, style: Theme.of(context).textTheme.headline4),
-                  Container(
-                    margin: EdgeInsets.only(top: 5.0),
-                    child: Text(text),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemBuilder: _listItemBuilder,
+        itemCount: _kittens.length,
+        itemExtent: 60.0,
       ),
     );
   }
