51dc418b819c3d524e985562c5a903f108ac37ac
BennAjax <bennkeys1@gmail.com>
2020-07-01 12:50:32 +0100

    add MI Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,73 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image.network('http://www.pngplay.com/wp-content/uploads/2/Diamond-Transparent-Image.png'),
-        ),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/images/ochuko.png'),
+                ),
+                Text(
+                  'Benn Ajax',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'ANDROID DEVELOPER',
+                  style: TextStyle(
+                      color: Colors.teal[100],
+                      letterSpacing: 2.5,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceSansPro'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '+234 816 202 4903',
+                      style: TextStyle(
+                        color: Colors.teal[500]
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'bennkeys1@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal[500]
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
